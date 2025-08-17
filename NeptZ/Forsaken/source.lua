78a00c6a7f031142a587ac7ec431f77606bae4c6
fuckg1thub <sigmasigmaboy283@outlook.com>
2025-08-17 01:39:25 +0100

    Update source.lua

--- NeptZ/Forsaken/source.lua
+++ NeptZ/Forsaken/source.lua
@@ -3,6 +3,10 @@
 -- added misc tab (Allow Jump, 	No Fog, Reset Character, Rejoin)
 -- added inf jump (pretty shit lmao)
 -- fixed kill all not stopping after being stuck on the same player for more than 15 seconds
+-- added auto block 1x1x1x1 poups
+-- added auto start generator
+-- added generator nametags
+-- added auto coin flip
 
 _G.yeaican = false
 if not _G.yeaican then
@@ -13,7 +17,7 @@ if not _G.yeaican then
         _G.ialreadyloadedit = true
     end
 end
-writefile("banger.mp3", game:HttpGet("https://github.com/fuckg1thub/assets/raw/refs/heads/main/banger.mp3"))
+--writefile("banger.mp3", game:HttpGet("https://github.com/fuckg1thub/assets/raw/refs/heads/main/banger.mp3"))
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/fuckg1thub/Fluent-Inspired-UI/refs/heads/main/fiui.luau"))()
 local window = library.Window("NXP hub (V1)", "Forsaken", "rbxassetid://118689160394652", false, Color3.fromRGB(150, 63, 204))
@@ -59,7 +63,46 @@ generatorsSection.Toggle("Generators ESP", function(bool)
         end
     end)
 end)
-generatorsSection.Toggle("Instant Generator", function(bool)
+generatorsSection.Toggle("Generators Nametags", function(bool)
+    _G.generatorstag = bool
+    task.spawn(function()
+        while task.wait() do
+            if _G.generatorstag then
+                local suc, res=  pcall(function()
+                    for i, v in pairs(workspace.Map.Ingame.Map:GetChildren()) do
+                        if v.Name == "Generator" and not v:FindFirstChild("nametag") then
+                            local bb = Instance.new("BillboardGui", v)
+                            bb.Size = UDim2.new(4, 0, 1, 0)
+                            bb.AlwaysOnTop = true
+                            bb.Name = "nametag"
+                            local text = Instance.new("TextLabel", bb)
+                            text.TextColor3 = Color3.fromRGB(255, 255, 255)
+                            text.TextStrokeTransparency = 0
+                            text.Text = "Generator (" .. v.Progress.Value .. "%)"
+                            text.TextSize = 20
+                            text.BackgroundTransparency = 1
+                            text.Size = UDim2.new(1, 0, 1, 0)
+                        elseif v:FindFirstChild("nametag") and v.Name == "Generator" then
+                            v.nametag.TextLabel.Text = "Generator (" .. v.Progress.Value .. "%)"
+                        end
+                    end
+                end)
+                print(suc, res)
+            else
+                pcall(function()
+                    for i, v in pairs(workspace.Map.Ingame.Map:GetChildren()) do
+                        if v.Name == "Generator" and v:FindFirstChild("nametag") then
+                            v.nametag:Destroy()
+                        end
+                    end
+                end)
+                break
+            end
+        end
+    end)
+end)
+local activelyAutoing = false
+generatorsSection.Toggle("Auto Complete Generator", function(bool)
     _G.instantGenerator = bool
     task.spawn(function()
         while _G.instantGenerator and task.wait() do
@@ -69,10 +112,12 @@ generatorsSection.Toggle("Instant Generator", function(bool)
                         if not generatorsDid[v] and v.Name == "Generator" then
                             generatorsDid[v] = true
                             local old; old = hookfunction(getsenv(v.Scripts.Client).toggleGeneratorState, function(a)
+                                if checkcaller() then return old(a) end
                                 if not _G.instantGenerator then return old(a) end
                                 if a ~= "enter" then return old("leave") end
                                 local ou = v.Remotes.RF:InvokeServer("enter")
                                 if ou ~= "fixing" then return end
+                                activelyAutoing = true
                                 for i = 1, 4 do
                                     if v.Progress.Value >= 100 then break end
                                     game.StarterGui:SetCore("SendNotification",
@@ -80,6 +125,7 @@ generatorsSection.Toggle("Instant Generator", function(bool)
                                     v.Remotes.RE:FireServer()
                                     task.wait(1.2)
                                 end
+                                activelyAutoing = false
                                 return ""
                             end)
                         end
@@ -89,6 +135,43 @@ generatorsSection.Toggle("Instant Generator", function(bool)
         end
     end)
 end)
+
+generatorsSection.Toggle("Auto Start Generator", function(bool)
+    _G.autoGen = bool
+    task.spawn(function()
+        while _G.autoGen and task.wait() do
+            if workspace.Map.Ingame:FindFirstChild("Map") then
+                pcall(function()
+                    for i, v in pairs(workspace.Map.Ingame.Map:GetChildren()) do
+                        if v.Name == "Generator" then
+                            pcall(function ()
+                                local function continue()
+                                    if game.Players.LocalPlayer.PlayerGui:FindFirstChild("PuzzleUI") then return end
+                                    if activelyAutoing then return end
+                                    if v.Main:FindFirstChild("Prompt") then
+                                        fireproximityprompt(v.Main.Prompt)
+                                    end
+                                    task.wait(1)
+                                end
+                                local hello = v.Positions.Center.Position
+                                local hello2 = v.Positions.Right.Position
+                                local hello3 = v.Positions.Left.Position
+                                if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - hello).magnitude <= 4 then
+                                    continue()
+                                elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - hello2).magnitude <= 4 then
+                                    continue()
+                                elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - hello3).magnitude <= 4 then
+                                    continue()
+                                end
+                            end)
+                        end
+                    end
+                end)
+            end
+        end
+    end)
+end)
+-- game:GetService("Players").LocalPlayer.PlayerGui.TemporaryUI:GetChildren()[12]
 
 killersSection.Toggle("Killer ESP", function(bool)
     _G.killers = bool
@@ -137,13 +220,21 @@ survivorsSection.Toggle("Survivors ESP", function(bool)
         end
     end)
 end)
+survivorsSection.Toggle("Auto Coin Flip", function (cool)
+    _G.coin = cool
+    task.spawn(function ()
+        while _G.coin and task.wait(2.1) do
+            game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Network"):WaitForChild("RemoteEvent"):FireServer("UseActorAbility", "CoinFlip")
+        end
+    end)
+end)
 itemsSection.Toggle("Items ESP", function(bool)
     _G.items = bool
     task.spawn(function()
         while task.wait() do
             if _G.items == true then
                 pcall(function()
-                    for i, v in pairs(workspace.Map.Ingame.Map:GetChildren()) do
+                    for i, v in pairs(workspace.Map.Ingame:GetDescendants()) do
                         if v:IsA("Tool") and not v:FindFirstChild("iskiddedfromneptz") then
                             local hl = Instance.new("Highlight", v)
                             hl.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
@@ -186,7 +277,7 @@ speedSection.Toggle("Speed Toggle", function (s)
             if not _G.mhhmmm then break end
             local humanoid = localPlayer.Character and localPlayer.Character:FindFirstChild("Humanoid")
             if humanoid and humanoid.MoveDirection ~= Vector3.zero then
-                localPlayer.Character:TranslateBy(humanoid.MoveDirection * (yeahvariable / 10) * game:GetService("RunService").RenderStepped:Wait() * 10)
+                localPlayer.Character:TranslateBy(humanoid.MoveDirection * yeahvariable * game:GetService("RunService").RenderStepped:Wait())
             end
         end
     end)
@@ -231,6 +322,33 @@ end)
 
 local killerTab = window:Tab("Killer", "rbxassetid://83038806046146")
 local killerSection = killerTab:AddSection("Killer")
+killerSection.Toggle("Allow Killer Entrances as survivor", function (call)
+    _G.killerent = call
+    local function s9audioak()
+        for i, v in pairs(workspace.Map.Ingame.Map.KillerOnlyEntrances:GetChildren()) do
+            v.CanCollide = true
+        end
+    end
+    if not _G.killerent then
+        pcall(s9audioak)
+        return
+    end
+    task.spawn(function ()
+        while _G.killerent and task.wait() do
+            if not workspace.Map.Ingame:FindFirstChild("Map") then continue end
+            if not workspace.Map.Ingame.Map:FindFirstChild("KillerOnlyEntrances") then continue end
+            if not _G.killerent then
+                pcall(s9audioak)
+                break
+            end
+            pcall(function ()
+                for i, v in pairs(workspace.Map.Ingame.Map.KillerOnlyEntrances:GetChildren()) do
+                    v.CanCollide = false
+                end
+            end)
+        end
+    end)
+end)
 killerSection.Toggle("Spectate Killer", function (state)
     if state then
         local killer = workspace.Players.Killers:GetChildren()[1]
@@ -321,7 +439,19 @@ miscSection.Button("Rejoin", function ()
         game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.localPlayer)
     end)
 end)
-miscSection.Button("play tuff musik 2025", function ()
+miscTab:AddSection("Popups").Toggle("Auto Remove 1x1x1x1 popups", function (bool)
+    _G.no1x= bool
+    task.spawn(function ()
+        while _G.no1x and task.wait() do
+            local temp = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("TemporaryUI")
+            if temp and temp:FindFirstChild("1x1x1x1Popup") then
+                temp["1x1x1x1Popup"]:Destroy()
+                warn("yes its gone")
+            end
+        end
+    end)
+end)
+--[[
     game.StarterGui:SetCore("SendNotification",
         { Title = "banger musik", Text = "u have activated super secrekt banger music feature ✅✅ only u hear it btw", Duration = 20 })
     local sound = Instance.new("Sound", workspace)
@@ -329,4 +459,4 @@ miscSection.Button("play tuff musik 2025", function ()
     sound.EmitterSize = 5000
     sound.Looped = true
     sound:Play()
-end)
+end]]
